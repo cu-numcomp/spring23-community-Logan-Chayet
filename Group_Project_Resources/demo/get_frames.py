f15520ae74d0aedd689968ca5d40340dc8a2d128
cast9559 <cast9559@colorado.edu>
2023-04-27 18:18:31 -0600

    all the files and updated get_frames.py

--- Group_Project_Resources/demo/get_frames.py
+++ Group_Project_Resources/demo/get_frames.py
@@ -5,25 +5,30 @@ def get_frames(video_file, save_dir):
     total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     middle_frame = total_frames // 2
 
-    # Get first frame
-    cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
-    ret, true_frame_first = cap.read()
-    cv2.imwrite(f"{save_dir}/one.png", true_frame_first)
+    # Get every frame
+    for i in range(total_frames):
+        cap.set(cv2.CAP_PROP_POS_FRAMES, i)
+        ret, true_frame = cap.read()
+        cv2.imwrite(f"{save_dir}/true_wheel_{i}.png", true_frame)
 
-    # Get middle frame
-    cap.set(cv2.CAP_PROP_POS_FRAMES, middle_frame)
-    ret, true_frame_middle = cap.read()
-    cv2.imwrite(f"{save_dir}/true_frame_middle.png", true_frame_middle)
+    # # Get first frame
+    # cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+    # ret, true_frame_first = cap.read()
+    # cv2.imwrite(f"{save_dir}/one.png", true_frame_first)
 
-    # Get last frame
-    cap.set(cv2.CAP_PROP_POS_FRAMES, total_frames - 1)
-    ret, true_frame_last = cap.read()
-    cv2.imwrite(f"{save_dir}/two.png", true_frame_last)
+    # # Get middle frame
+    # cap.set(cv2.CAP_PROP_POS_FRAMES, middle_frame)
+    # ret, true_frame_middle = cap.read()
+    # cv2.imwrite(f"{save_dir}/true_frame_middle.png", true_frame_middle)
 
+    # # Get last frame
+    # cap.set(cv2.CAP_PROP_POS_FRAMES, total_frames - 1)
+    # ret, true_frame_last = cap.read()
+    # cv2.imwrite(f"{save_dir}/two.png", true_frame_last)
     cap.release()
 
 
 
-mp4_file = '/Users/nicolasperrault/Desktop/Results/SingleGearSmallMovement/TrueVideoSingleGear.mp4'
-download_path = '/Users/nicolasperrault/Desktop/Results/SingleGearSmallMovement'
+mp4_file = '../Results/SingleGearSmallMovement/TrueVideoSingleGear.mp4'
+download_path = '../Results/SingleGearSmallMovement'
 get_frames(mp4_file, download_path)
